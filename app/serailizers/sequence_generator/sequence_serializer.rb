c0c057f968d0df756c25d4c0834f18662e2c6bd3
vimox-shah <vimox@shipmnts.com>
2018-03-31 22:49:12 +0530

    errros add

--- app/serailizers/sequence_generator/sequence_serializer.rb
+++ app/serailizers/sequence_generator/sequence_serializer.rb
@@ -1,5 +1,6 @@
 module SequenceGenerator
   class SequenceSerializer < BaseSerializer
-
+    attributes :id, :sequence_prefix, :sequential_id, :start_at, :valid_from, :valid_till,
+    :reset_from_next_year, :purpose, :financial_year_start, :financial_year_end, :scope
   end
 end
