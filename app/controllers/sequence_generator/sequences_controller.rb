68a65be0c36880de2e54fd76703d0b8318d5e930
vimox-shah <vimox@shipmnts.com>
2018-04-09 21:55:16 +0530

    sequence fetch api complete

--- app/controllers/sequence_generator/sequences_controller.rb
+++ app/controllers/sequence_generator/sequences_controller.rb
@@ -25,6 +25,51 @@ module SequenceGenerator
       end
     end
 
+    def index
+        sequence = Sequence.where(purpose: params[:purpose], scope: params[:scope])
+                           .where('valid_from <= ? AND valid_till >= ?', Time.now, Time.now).first
+        if sequence
+          sequence_number = sequence.generate_sequence_number
+          render json: { sequence_number: sequence_number, purpose: params[:purpose],
+                         scope: params[:scope] }, status: :ok
+        else
+          original_sequence = Sequence.where(purpose: params[:purpose], scope: params[:scope]).last
+          if original_sequence.nil?
+            api_error(status: :unprocessable_entity,
+                      message: 'Sequence is not Created')
+          else
+            valid_from = original_sequence.valid_from
+            valid_till = original_sequence.valid_till
+            new_start_at = original_sequence.sequential_id
+            difference = (valid_till - valid_from).to_i
+            new_valid_from = Date.today
+            new_valid_till = new_valid_from + difference
+            if original_sequence.reset_from_next_year
+              sequence = Sequence.create!(original_sequence.as_json.except('id', 'start_at',
+                                                                           'valid_from', 'valid_till',
+                                                                           'sequential_id',
+                                                                           'created_at', 'updated_at')
+                                              .merge!(start_at: 1,
+                                                      valid_from: new_valid_from,
+                                                      valid_till: new_valid_till,
+                                                      sequential_id: 0))
+            else
+              sequence = Sequence.create!(original_sequence.as_json.except('id', 'start_at',
+                                                                           'valid_from', 'valid_till',
+                                                                           'sequential_id',
+                                                                           'created_at', 'updated_at')
+                                              .merge!(start_at: new_start_at,
+                                                      valid_from: new_valid_from,
+                                                      valid_till: new_valid_till,
+                                                      sequential_id: new_start_at))
+            end
+            sequence_number = sequence.generate_sequence_number
+            render json: { sequence_number: sequence_number, purpose: params[:purpose],
+                           scope: params[:scope] }, status: :ok
+          end
+        end
+    end
+
     private
 
     def transactions_filter
--- app/models/sequence_generator/sequence.rb
+++ app/models/sequence_generator/sequence.rb
@@ -11,7 +11,7 @@ module SequenceGenerator
 
     def generate_sequence_number
       sequence_number = "%05d" % (sequential_id + 1).to_s
-      generated_sequential_id = "#{sequence_prefix}#{"/"}#{sequence_number}"
+      generated_sequential_id = "#{sequence_prefix}#{sequence_number}"
       self.update(sequential_id: sequential_id + 1)
       return generated_sequential_id
     end
