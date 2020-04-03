81c8631dc7a69f6884423cbbfb260e2ea139b8b9
Prasann Shah <prasann@shipmnts.com>
2020-04-04 02:40:54 +0530

    use date column if passed as option

--- app/models/concerns/act_as_sequenced.rb
+++ app/models/concerns/act_as_sequenced.rb
@@ -58,8 +58,12 @@ module SequenceGenerator
             errors.add(:sequential_id, 'Sequence is not associated with your branch')
           end
         else
+          date_to_consider = Time.now
+          if options[:date_column] && send(options[:date_column])
+            date_to_consider = send(options[:date_column])
+          end
           sequence = Sequence.where(purpose: options[:purpose], scope: send(options[:scope]))
-                         .where('valid_from <= ? AND valid_till >= ?', Time.now, Time.now).first.lock!
+                         .where('valid_from <= ? AND valid_till >= ?', date_to_consider, date_to_consider).first.lock!
           unless self.as_json[options[:column]].present?
             if sequence
               assign_attributes(options[:column]=> sequence.generate_sequence_number)
